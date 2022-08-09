4700c8bfa4c8acd1b6998d6cfeec94230a4b7625
Grundy <urffin2006@gmail.com>
2022-08-09 13:54:58 +0300

    first

---
+++ out/_next/static/rBzYQKRFYzxbSn4R0opvk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7e27fa6bcb139f5c.css","static/chunks/pages/index-e687f0d39903364d.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
